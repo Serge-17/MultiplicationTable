ca5422388dbeba38d139e4ffd5182d6aca44ac7d
Serge-17 <sergei.eliseev17@icloud.com>
2024-06-24 11:19:02 +0300

    Initial Commit

---
+++ MultiplicationTable/MultiplicationTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTableApp.swift
+//  MultiplicationTable
+//
+//  Created by Serge Eliseev on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MultiplicationTableTests/MultiplicationTableTests.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTableTests.swift
+//  MultiplicationTableTests
+//
+//  Created by Serge Eliseev on 6/24/24.
+//
+
+import Testing
+@testable import MultiplicationTable
+
+struct MultiplicationTableTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
